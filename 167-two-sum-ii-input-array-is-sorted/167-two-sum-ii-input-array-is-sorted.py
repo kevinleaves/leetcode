14a1c66859b1ae90fccbe6631776f8a30330e5da
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-02-18 11:41:52 -0800

    Time: 192 ms (15.34%), Space: 14.8 MB (25.58%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -5,14 +5,14 @@ class Solution:
         #while loop runs when l <= r
         l, r = 0, len(numbers)-1
         while l <= r:
-            if numbers[l] + numbers[r] == target:
+            current_sum = numbers[l] + numbers[r]
+            if current_sum == target:
                 return [l+1,r+1]
-            elif numbers[l] + numbers[r] < target:
+            elif current_sum < target:
                 l += 1
-                print(f"l = {l}")
             else:
                 r -= 1
-                print(f"r = {r}")
+                
         
         
                 
