c332457a13a9ace384b55a59fad3b96dac496722
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-03-01 19:52:03 -0800

    Time: 168 ms (90.76%), Space: 15.6 MB (45.55%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,32 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # l, r = 0, len(nums)-1
-        # while l <= r:
-        #     if l == 0 and r != 0:
-        #         nums[l], nums[r] = nums[r], nums[l]
-        #         r -= 1
-        #     if l!=0 and r!=0:
-        #         nums[l], nums[r] = nums[r], nums[l]
-        #         l+=1
-        #     l+=1
-        # print(nums)
-#         from collections import deque
-        
-#         arrSorted = collections.deque([])
-#         for i in nums:
-#             if i==0:
-#                 arrSorted.append(i)
-#             else:
-#                 arrSorted.appendleft(i)
-#         print(arrSorted)
-        
-        count = 0
-        for i in nums:
-            if i != 0:
-                nums[count] = i
-                count += 1
-        while count < len(nums):
-            nums[count] = 0
-            count+=1
+        # count = 0
+        # for i in nums:
+        #     if i != 0:
+        #         nums[count] = i
+        #         count += 1
+        # while count < len(nums):
+        #     nums[count] = 0
+        #     count+=1
+
+            
+        l = 0
+        #move all nonzero values to the left
+        for r in range(0, len(nums)):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
+                
             
