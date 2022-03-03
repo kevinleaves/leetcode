7d29d9264b4409822b49fcbb13fff225a4fa4fb6
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-03-03 11:45:48 -0800

    Time: 183 ms (78.01%), Space: 15.6 MB (16.13%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,21 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # count = 0
-        # for i in nums:
-        #     if i != 0:
-        #         nums[count] = i
-        #         count += 1
-        # while count < len(nums):
-        #     nums[count] = 0
-        #     count+=1
-
-            
+        #move all 0's to the end of the array
+        #OR move all nonzero to the start
+        #in-place
+        #2 pointers
         l = 0
-        #move all nonzero values to the left
-        for r in range(0, len(nums)):
-            if nums[r] != 0:
+        for r, value in enumerate(nums):
+            if value:
                 nums[l], nums[r] = nums[r], nums[l]
                 l+=1
                 
-            
