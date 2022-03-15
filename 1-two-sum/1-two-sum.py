df79c2ec611983540bea93750b1f6cdf39821662
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-03-14 21:05:29 -0700

    Time: 56 ms (97.82%), Space: 15.1 MB (54.37%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,14 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #use a hashmap to track seen
         seen = {}
-        
-        #iterate through array
-        #test for condition if target - value is in the hashmap
-        #return indices of 2 numbers in res array
         for idx, value in enumerate(nums):
             if target - value in seen:
-                return seen[target-value], idx
+                return [idx,seen[target-value]]
             else:
                 seen[value] = idx
-        
         
