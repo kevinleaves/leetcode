cb974b157abb20bace3abd80cee2e48b6f822e3a
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-03-17 12:23:13 -0700

    Time: 770 ms (85.41%), Space: 27.8 MB (97.22%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -3,10 +3,8 @@ class Solution:
         maxSum = nums[0]
         currSum = 0
         for n in nums:
-            print(f"n = {n}")
             if currSum < 0:
                 currSum = 0
             currSum += n
             maxSum = max(currSum, maxSum)
-            print(f"currSum = {currSum}")
         return maxSum
