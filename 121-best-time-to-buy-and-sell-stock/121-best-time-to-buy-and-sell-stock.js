ac4871e41f5139addcd8007ec75c0206d36cc55a
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-07-05 15:55:56 -0700

    Time: 6293 ms (5.03%), Space: 66.4 MB (5.04%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  //create two pointers (high within the loop)
+  //create max variable to hold max
+  var max = 0
+  var low = 0
+  
+  //iterate through array
+  for (var high = 0; high < prices.length ; high++) {
+    
+    var difference = prices[high] - prices[low];
+    console.log(difference)
+    console.log("low",low)
+    //shift low pointer
+    if (prices[high] < prices[low]) {
+      low = high
+    }
+    
+    // if (difference < 0) {
+    //   low = high;
+    // }
+    
+    //finding max
+    if (difference > max) {
+        max = difference;
+    }
+  }
+
+  return max
+};
+
