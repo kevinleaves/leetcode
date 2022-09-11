7657320d1ad6bc1f77acb473ad4eec2452138be4
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-09-11 12:43:19 -0700

    Time: 60 ms (97.07%), Space: 42.2 MB (52.44%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -5,23 +5,23 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-  //set pointers, m, n, m+n
-  var last = m + n - 1;
-  m = m-1
-  n = n-1
-  // console.log(last, m, n)
-  //iterate backwards through nums1
-  while (n >= 0) {
-    if (nums1[m] > nums2[n]) {
-      nums1[last] = nums1[m]
-      m--
-    } else {
-      nums1[last] = nums2[n]
-      n--
+var merge = function(nums1, m, nums2, n) { //[0], 0, [1], 1
+    //3 pointers
+    let pos = m + n - 1 //0
+    m = m - 1; //-1
+    n = n - 1; //0
+    
+//     //we must traverse entirety of the second array from the right side
+    while (n >= 0) { //n = 0 true
+        //compare largest number of 2nd array & the first array
+        if (nums1[m] > nums2[n]) {
+            nums1[pos] = nums1[m];            
+            m--;
+        } else {
+            nums1[pos] = nums2[n];
+            n--
+        }
+        pos--;
     }
-    last--
-  }
-  
-  
 };
+
