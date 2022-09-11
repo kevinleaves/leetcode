ffe6254850339d07c354d983e6bc7f753c84b521
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-09-11 12:51:26 -0700

    Time: 118 ms (56.74%), Space: 42.1 MB (86.36%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,25 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    //create seen map
-    //create result array
-    var seen = {};
-    var result = []; 
-    
-    //iterate through array
-    for (var i = 0; i < nums.length; i++) {
-      var currentNumber = nums[i];
-      //if target - current number exists in seen as a key
-      if (seen[target - currentNumber] !== undefined) {
-        //push seen index and current index to result array
-        result.push(i)
-        result.push(seen[target - currentNumber])
-      } else {
-        // else store index and value inside seen map as value: index
-        seen[currentNumber] = i
-      }
-    }
-  
-  //return result array
-  return result
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            } 
+        }
+    }   
 };
+
