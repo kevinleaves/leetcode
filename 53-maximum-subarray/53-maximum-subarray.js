a2d50ded542270613c340dc552144617398d5945
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-09-15 11:09:07 -0700

    Time: 142 ms (27.41%), Space: 50.4 MB (44.45%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,21 +3,26 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-  //l, r pointers
-  var result = nums[0]
-  var l = 0
-  var currSum = 0
-      
-  for (var r = 0; r < nums.length; r++) {
-    if (currSum < 0) {
-      l = r
-      currSum = 0
+    //current sum = first number
+    let maxSum = nums[0]; //5
+    //max sum = 0
+    let currSum = 0; //0
+    
+    // if (nums.length === 1) {
+    //     return currSum
+    // }
+    
+    //iterate through nums
+    for (let i = 0; i < nums.length; i++) {
+        if (currSum < 0) {
+            currSum = 0;
+        }
+        currSum += nums[i];
+        maxSum = Math.max(currSum, maxSum)
     }
-    currSum += nums[r]
-    result = Math.max(currSum, result)
-  }
-  
-  return result
+        //if current sum < 0, set it to 0
+        //current sum += num at current idx
+        //maxsum contains the max of currentsum and maxsum
+        
+    return maxSum
 };
-
-
