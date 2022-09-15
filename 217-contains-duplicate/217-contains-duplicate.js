efd121321eb42a18c05519cb94205a8bcc1ff63a
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-09-15 11:20:02 -0700

    Time: 3927 ms (5.01%), Space: 50.4 MB (80.21%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,16 +3,23 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-  //create obj
-  //iterate through array
-  //short circuit?
-  var seen = {};
-  for (var i = 0; i < nums.length; i++) {
-    var num = nums[i];
-    if (num in seen) {
-      return true
+    //create duplicate array
+    let copy = [];
+    //create boolean flag = false
+    let duplicate = false;
+    
+    //iterate through original array
+    for (let i = 0; i < nums.length; i++) {
+        if (copy.indexOf(nums[i]) === -1) {
+            copy.push(nums[i])
+        } else {
+            duplicate = true;
+        }
     }
-    seen[num] = seen[num] + 1 || 1 
-  }
-  return false
+        //if element isn't in duplicate array
+            //push it to duplicate
+        //else
+            //flip boolean flag to true
+    //return boolean
+    return duplicate
 };
