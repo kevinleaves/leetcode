62858ff7aa9090ca6695a34e2cc9be58bf58e713
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-09-23 13:32:38 -0700

    Time: 1932 ms (5.02%), Space: 51.3 MB (5.21%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    //freq table from magazine
+    let freq = {};
+    for (let i = 0; i < magazine.length; i++) {
+        console.log(magazine[i])
+        freq[magazine[i]] = (freq[magazine[i]] || 0) + 1;
+    }
+    //iterate through ransomnote
+    for (let i = 0; i < ransomNote.length; i++) {
+        //if current letter exists in magazine
+            //-1
+        //if it doesn't
+            //return false
+        if (freq[ransomNote[i]]) {
+            freq[ransomNote[i]]--
+        } else {
+            return false
+        }
+    }
+    
+    //return true if loop ends with no falses
+    return true
+        
+        
+    
+};
