809271644c8b5cb7a3949de54bddea364d757dc8
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-09-23 12:45:24 -0700

    Time: 90 ms (90.04%), Space: 42.3 MB (96.83%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,30 +4,27 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-  //writing out 2 false conditions, if none of them hit false, return true
-  //automatically false if two strings are different length
-  if (s.length !== t.length) {
-    return false
-  }
-  //popular counts dictionary with counts of all characters
-  const counts = {};
-  for (let char of s) {
-    if (!(char in counts)) {
-      counts[char] = 1  
-    } else {
-      counts[char] += 1
+    //create freq table for string s
+    //iterate through string t, for each char -1 char from freq table. if char doesn't exist return false if it does -1
+    
+    
+    //if freq table is empty at the end return true
+    let freq = {};
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        freq[char] = freq[char] + 1 || 1;
     }
-  }
-  
-  console.log(counts)
-  //iterate through second string, 
-  for (let c of t) {
-    if (!counts[c]) {
-      return false
-    }
-    counts[c]--;
-  }
-  console.log(counts)
-  //if no false conditions are satisfied, return true
-  return true
+    
+    for (let j = 0; j < t.length; j++) {
+        let char = t[j];
+        if (char in freq) {
+            freq[char] = freq[char] - 1
+        } else {
+            return false
+        }
+    }        
+    
+    const isEmpty = Object.values(freq).every(x => x === 0)
+    
+    return isEmpty
 };
