a05a77107e1d224ae49586bedd1ed36f15937fa9
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-09-26 11:18:52 -0700

    Time: 119 ms (47.22%), Space: 44.4 MB (74.97%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,12 +3,25 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-  //remove all non-ascii chars
-  //convert string to all lowercase
-
-  s = s.toLowerCase();
-  s = s.replace(/[^a-z0-9]/g, "");
-  
-  //return (comparison between reversed clean string and original cleaned string)
-  return s.split("").reverse().join("") === s
+    function isASCII (char) {
+        let pattern = /\w/g;
+        return pattern.test(char)
+    }
+    
+    //left and right pointers
+    //check if char at left and right are ===
+    //if they aren't return false
+    //if they are, left += 1, right -= 1
+    //if char is nonascii go next char
+    //loop breaks when l > r
+    
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '')
+                       
+    for (let i = 0, j = s.length-1; i <= j; i++, j--) {
+        if (s[i] !== s[j]) {
+            return false
+        }
+    }
+    
+    return true
 };
