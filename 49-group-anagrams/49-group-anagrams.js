6badd21cb72369bf91fa2130125b01072e716fd4
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-10-01 16:20:13 -0700

    Time: 344 ms (9.58%), Space: 55.2 MB (16.95%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -5,23 +5,15 @@
 var groupAnagrams = function(strs) {
   let mapping = {};
   for (let str of strs) {
-    //create freq table/hashtable for each string
     let hash = new Array(26).fill(0)
     for (let i = 0; i < str.length; i++) {
       hash[str[i].charCodeAt(0) - 'a'.charCodeAt(0)]++  
     }
-    
-    //add hashstrings as mapping keys to store matching strings
-    if (!mapping[hash]) {
+      if (!mapping[hash]) {
       mapping[hash] = [];
     }
-    
-    //push the matching strings to the value array
-    mapping[hash].push(str)
-    
+    mapping[hash].push(str)   
   }
-  
-  //this obj returns a nested array of obj values
   return Object.values(mapping)  
 };
 
