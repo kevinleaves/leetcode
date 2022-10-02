706b1610118556f91d36590e9c3b245d7c6354e6
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-10-02 11:32:43 -0700

    Time: 128 ms (55.34%), Space: 44.6 MB (45.22%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,27 +4,18 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    //create freq table for string s
-    //iterate through string t, for each char -1 char from freq table. if char doesn't exist return false if it does -1
-    
-    
-    //if freq table is empty at the end return true
-    let freq = {};
-    for (let i = 0; i < s.length; i++) {
-        let char = s[i];
-        freq[char] = freq[char] + 1 || 1;
+    //make hashstr for s
+    let hashS = new Array(26).fill(0)
+    for (let char of s) {
+      hashS[char.charCodeAt(0) - 'a'.charCodeAt(0)]++
     }
     
-    for (let j = 0; j < t.length; j++) {
-        let char = t[j];
-        if (char in freq) {
-            freq[char] = freq[char] - 1
-        } else {
-            return false
-        }
-    }        
-    
-    const isEmpty = Object.values(freq).every(x => x === 0)
-    
-    return isEmpty
+    //make hashstr for t
+    let hashT = new Array(26).fill(0)  
+    for (let char of t) {
+      hashT[char.charCodeAt(0) - 'a'.charCodeAt(0)]++
+    }
+  
+    //compare 2 hashstrs
+    return hashS.toString() === hashT.toString()
 };
