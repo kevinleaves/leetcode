580fba2b89394243c50584c2a0fdaf6feb0b0ab5
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-10-19 16:37:16 -0700

    Time: 131 ms (75.69%), Space: 46.3 MB (88.57%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    if (nums.length === 1) {
+        return nums
+    }
+    
+    let l = 0;
+    for (let r = 0; r < nums.length; r++) {      
+      if (nums[r] !== 0) {
+        [nums[l], nums[r]] = [nums[r], nums[l]]
+        l++
+      }
+    }
+  
+  return nums
+};
