4ae01ec7cece89bca80f2a16409545b2f2be48c0
Kevin Le <60834712+kevin-v-le@users.noreply.github.com>
2022-10-20 16:50:00 -0700

    Time: 202 ms (27.61%), Space: 48.3 MB (89.91%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -6,18 +6,17 @@ var sortedSquares = function(nums) {
   let l = 0;
   let r = nums.length-1;
   let sorted_idx = r;
-  let sorted = [];
+  let sorted = []
   
   while (sorted_idx > -1) {
     if (Math.abs(nums[r]) < Math.abs(nums[l])) {
       sorted[sorted_idx] = nums[l] * nums[l];
       l++
-      sorted_idx--
     } else {
       sorted[sorted_idx] = nums[r] * nums[r];
       r--
-      sorted_idx--
     }
+    sorted_idx--
   }  
   return sorted
 };
