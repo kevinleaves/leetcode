158461108c20482cb92d6d39b58d44788056a838
Kevin Le <iamkevinvle@gmail.com>
2023-04-07 23:08:42 -0700

    121

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -11,6 +11,21 @@ class Solution:
                 start = end
             res = max(res, diff)
         return res
-            
-        
-        
+
+class Solution: # 71.82% 80.39%
+    def maxProfit(self, prices: List[int]) -> int:
+        # min is usually initialized as the first value of an iterable
+        minPrice = prices[0]
+        # this is our result
+        maxPrice = 0
+
+        for i in range(len(prices)):
+            currPrice = prices[i]
+            # compare current price to the current minimum. we want to find the largest range so we track min
+            if currPrice < minPrice:
+                minPrice = currPrice
+            # calculate max possible price
+            maxPrice = max(maxPrice, currPrice - minPrice)
+        return maxPrice
+
+
