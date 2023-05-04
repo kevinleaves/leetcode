2d6f7e56f78e45dc931fa6e8a87b173949d310e7
Kevin Le <iamkevinvle@gmail.com>
2023-05-04 12:14:05 -0700

    155

--- 155-Min Stack/155-Min-Stack.py
+++ 155-Min Stack/155-Min-Stack.py
@@ -1,3 +1,4 @@
+# 16.92% 6.45%
 class MinStack:
     def __init__(self):
         self.data = []
@@ -23,6 +24,55 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.data[-1][1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+# FAILED ATTEMPT. OVERCOMPLICATED THINGS & USED A DICT TO INSTANTIATE. can probably make this work tho. same concept applies
+class MinStack:
+    def __init__(self):
+        self.data = {}
+        # store k:v pair for the min value in stack. update this array as we push. lookup
+        self.min = []
+        self.index = 0
+
+    def push(self, val: int) -> None:
+        # return nothing
+        self.data[self.index] = val
+
+        # update min
+        if not self.min:
+            self.min.append(self.index)
+            self.min.append(val)
+        else:
+            if val < self.min[1]:
+                self.min[0] = self.index
+                self.min[1] = val
+
+        # update index
+        self.index += 1
+
+    def pop(self) -> None:
+        # return nothing
+        self.index -= 1
+        popped = self.data[self.index]
+        del self.data[self.index]
+        # if i deleted the minimum, update the val
+        # if popped == self.min[1]:
+
+    def top(self) -> int:
+        # return int
+        return self.data[self.index - 1]
+
+    def getMin(self) -> int:
+        # return int
+        return self.min[1]
 
 
 # Your MinStack object will be instantiated and called as such:
