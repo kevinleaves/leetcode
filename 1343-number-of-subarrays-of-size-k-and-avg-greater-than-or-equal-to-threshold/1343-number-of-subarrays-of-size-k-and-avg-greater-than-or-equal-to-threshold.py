2cf8a2113c40bd9e3b6c667b6fc205c2a6d8b3f3
Kevin Le <iamkevinvle@gmail.com>
2023-06-01 14:14:24 -0700

    1343 more efficient

--- 1343-number-of-subarrays-of-size-k-and-avg-greater-than-or-equal-to-threshold/1343-number-of-subarrays-of-size-k-and-avg-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-subarrays-of-size-k-and-avg-greater-than-or-equal-to-threshold/1343-number-of-subarrays-of-size-k-and-avg-greater-than-or-equal-to-threshold.py
@@ -23,3 +23,32 @@ class Solution:
                 if avg >= threshold:
                     res += 1
         return res
+
+
+# 79.43% 31.27%
+# checks edge case + initializes a total + valid subarray right away
+# every iteration of the loop slides window. calculates back of the window using the right pointer
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        res = 0
+        # edge case: if length of arr < k, no valid subarrays
+        if len(arr) < k:
+            return res
+        # initialize total at a valid k to immediately skip over subarrays < size k
+        windowTotal = sum(arr[:k])
+        # check initial total
+        if windowTotal >= threshold * k:
+            res += 1
+
+        # begin a loop starting from k to the end of the array, sliding the window at every iteration
+        for r in range(k, len(arr)):
+            # add right to total
+            windowTotal += arr[r]
+            # increment left/contract the window/total. calculate the left of the window using the right pointer
+            windowTotal -= arr[r - k]
+
+            # check if window is valid
+            if windowTotal >= threshold * k:
+                res += 1
+
+        return res
