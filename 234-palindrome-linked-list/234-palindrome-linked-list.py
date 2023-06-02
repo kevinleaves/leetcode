aff2070e22655c685289e7c798373ddd8fa29645
Kevin Le <iamkevinvle@gmail.com>
2023-06-02 11:49:27 -0700

    234-palindrome-linked-list O(1) space

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -16,3 +16,34 @@ class Solution:
             res.append(curr.val)
             curr = curr.next
         return res[::-1] == res
+
+
+# 93.9% 61.94%
+# key to this solution: find the midpoint node using fast/slow algorithm. reverse the LL starting from that point to find the tail. use head and tail to check for palindrome. remember that when you reverse the rightmost portion of the linked list, the midpoint points to a Null previous node, so that is the condition for when your palindrome check loop ends.
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # traverse through linked list, build an array from the linked list values
+        # use array of values to compute boolean
+        fast = head
+        slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # we've found the middle, reverse back half of LL
+        prev = None
+        while slow:
+            next = slow.next
+            slow.next = prev
+            prev = slow
+            slow = next
+
+        # prev is now pointing towards our tail. is flowing <-
+        # while loop comparing values at both ends. check for palindrome
+        while head and prev:
+            if head.val != prev.val:
+                return False
+            head = head.next
+            prev = prev.next
+        return True
