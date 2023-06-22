8f688455847eb45f931cc525f4d41eaafa441b06
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2023-06-22 11:10:51 -0700

    Time: 166 ms (86.84%), Space: 16.9 MB (26.57%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,18 +1,12 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         idx = 1
-        flowerbed2 = [0] + flowerbed + [0]
+        f = [0] + flowerbed + [0]
         
-        while idx < len(flowerbed2) - 1:
-            if flowerbed2[idx] == 1:
-                idx += 2
-            else:
-                # check adjacent plots for 1's
-                if flowerbed2[idx - 1] != 1 and flowerbed2[idx + 1] != 1:
-                    # we can fill 
-                    flowerbed2[idx] = 1
-                    n -= 1
-                    idx += 2
-                else:
-                    idx += 1
+        for i in range(1, len(f) - 1): # skip first and last
+            # if 3 consecutive 0's, we can fill
+            if f[i - 1] == 0 and f[i] == 0 and f[i + 1] == 0:
+                f[i] = 1
+                n -= 1
         return n <= 0
+
