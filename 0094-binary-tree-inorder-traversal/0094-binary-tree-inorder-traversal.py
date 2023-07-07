5bb7cb0ed7014ccf5efc1434bb32d06be80eaa8d
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2023-07-07 11:26:46 -0700

    Time: 49 ms (39.75%), Space: 16.2 MB (76.80%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -7,18 +7,26 @@
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
+        stack = []
+        
+        # use a pointer to traverse in iterative solution
+        curr = root    
+        # consider this loop the equivalent of a recursive call
+        while stack or curr:
+            # this while loop is essentially a base case
+            # go left as much as we can
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            
+            # curr has reached all the way to the left/curr is null, we can perform actions now
+            # top of the stack is our leftmost node
 
-        def dfs(node):
-            # base case(s)
-            if not node:
-                return
-            nonlocal res
-
-            # recursive case(s)
-            dfs(node.left)
-            res.append(node.val)
-            dfs(node.right)
-
-        dfs(root)
+            # a stack pop is basically a recursion return. every time we hit a null we pop
+            curr = stack.pop()
+            # inorder traversal
+            res.append(curr.val)
+            curr = curr.right
+            
         return res
 
