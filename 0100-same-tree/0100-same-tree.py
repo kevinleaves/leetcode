cd011e08759195f9cdb8190494bf7b38d36bb9ad
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2023-07-15 14:21:51 -0700

    Time: 48 ms (50.02%), Space: 16.4 MB (58.09%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -7,21 +7,22 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         
-        def dfs(node1, node2):
-            # base case(s)
-            if not node1 and not node2:
-                return True
-            if not node1 or not node2:
-                return False
-
-            equalVals = node1.val == node2.val    
-            left = dfs(node1.left, node2.left)
-            right = dfs(node1.right, node2.right)
-
-            # recursive case(s)
-            # if root has equal vals, check left + r subtree
-            return equalVals and left and right
+        '''
+        starting at the root dfs to traverse both trees simultaneously. if ever their values are !=, return false upwards. if any falses, return false
+        '''
         
-        return dfs(p, q)
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        # compare them
+        if p.val != q.val:
+            return False
 
-            
+        left = self.isSameTree(p.left, q.left)
+        right = self.isSameTree(p.right, q.right)
+
+        return left and right
+
+    
+        
