5d3c93e8efb97ca8b4446ae5f1f3314d601e9750
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2023-07-15 12:51:35 -0700

    Time: 56 ms (72.80%), Space: 17.5 MB (81.91%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,36 +6,20 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        '''
-        i: root, target int
-        o: boolean representing whether the tree contains a root to leaf path whos vals sum==target
-        c: none
-        e: empty tree => false
-
-        only do target comparison if we've arrived at a leaf node
-        '''
-        if not root:
-            return False
-
         def dfs(node, currPathSum):
-            # base case(s)
             if not node:
-                return (False, 0)
+                return False
             
             currPathSum += node.val
-            
-            # recursive case(s)
+
+            # is leaf, do the comparison
+            if not node.left and not node.right:
+                # do the comparison
+                return currPathSum == targetSum
+
             left = dfs(node.left, currPathSum)
             right = dfs(node.right, currPathSum)
 
-            # only if leaf node
-            isLeafNode = not left[0] and not right[0] and left[1] == 0 and right[1] == 0
-            if isLeafNode and currPathSum == targetSum:
-                return (True, currPathSum)
-            else:
-                return (left[0] or right[0], currPathSum)
-        return dfs(root, 0)[0]
+            return left or right
 
-
-
-        
+        return dfs(root, 0)
