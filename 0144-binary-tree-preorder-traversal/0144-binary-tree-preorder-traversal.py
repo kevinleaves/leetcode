683126ad1bf864040d8f9ed8020c8224535de7b3
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2023-07-19 12:53:07 -0700

    Time: 46 ms (62.29%), Space: 16.4 MB (40.27%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -6,38 +6,37 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-      '''
-      iterative dfs (simulate recursion)
-      add, left, right
-      '''
-      res = []
-      stack = []
-      # use a pointer to emulate recursive tree traversal
-      curr = root
+        # node, left, right
+        # process first, explore left nodes, explore right nodes.
+        # iterative => emulate recursion using a stack + pointer
 
-      while curr or stack:
-        # traverse left but add right node first
-        if curr:
-          res.append(curr.val)
-          stack.append(curr.right)
-          curr = curr.left
-        else:
-          curr = stack.pop()
-        
-      return res
+        res = []
+        curr = root
+        stack = []
 
+        '''
+        [1 2 4 5 6]
+        res = [1 2 4]
+        stack =[3 5]
+         1
+        2 3
+       4 5
+          6
 
-      '''
-      res = []
-      def dfs(root):
-        # base case(s)
-        if not root:
-          return
-        # recursive case(s)
-        res.append(root.val)
-        dfs(root.left)
-        dfs(root.right)
-      
-      dfs(root)
-      return res
-      '''
+        where i went wrong? i was missing the condition of when curr == None. didn't identify that i either move left OR i pop to return right. we're also able to add null nodes here because we take care of that scenario in our base case
+        '''
+        while stack or curr:
+            # when do we pop? when we're done going left. notice we have no while loops here
+            if not curr:
+                curr = stack.pop()
+            else:
+                # process node
+                res.append(curr.val)
+                
+                # add right node so we can pop back to right
+                stack.append(curr.right)
+
+                # go left. if curr == none that's base case, then we pop and go right
+                curr = curr.left
+
+        return res
