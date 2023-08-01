f6ee81b43be8f35cdd25ad1d27052ffa00c768d0
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2023-08-01 12:36:32 -0700

    Time: 142 ms (22.22%), Space: 17.8 MB (10.44%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,42 +6,34 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        '''
-        i: root and subRoot
-        o: boolean representing whether root has a subtree with the exact same structure and node values of subRoot. true if it does, false if it does not
-        c: 
-        e: subroot will always be at least 1 node
-        '''
-        def sameTree(node1, node2):
-            # base case(s)
-            if not node1 and not node2:
+
+        def sameTree(s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
+            if not s and not t:
                 return True
-            if not node1 or not node2:
+            if not s or not t:
                 return False
-
-            equalVals = node1.val == node2.val    
-            left = sameTree(node1.left, node2.left)
-            right = sameTree(node1.right, node2.right)
-
-            # recursive case(s)
-            # if root has equal vals, check left + r subtree
-            return equalVals and left and right
-
-        if not subRoot:
-            return True
+            if s.val != t.val:
+                return False
+            
+            left = sameTree(s.left, t.left)
+            right = sameTree(s.right, t.right)
+            return left and right
+        
+        # base case(s)
         if not root:
             return False
-        if sameTree(root, subRoot):
+        if not subRoot:
             return True
-        else:
+
+        res = sameTree(root, subRoot)
+
+        if not res:
+            # recursive case(s)
             left = self.isSubtree(root.left, subRoot)
             right = self.isSubtree(root.right, subRoot)
-            return left or right 
-         
+            return left or right
+        else:
+            return True
 
-      
 
         
-    
-        
-
