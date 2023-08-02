4cdda75d61304c8fdc0881da412d39dd2f14d550
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2023-08-02 10:24:42 -0700

    Time: 173 ms (63.57%), Space: 91 MB (12.22%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -7,42 +7,29 @@
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         '''
+        preorder[0] gives us the root node [3]
+        find 3 within inorder array, its left subarray is left tree, right subarray is right tree
+        preorder is node, left, right, use inorder subarray lengths to split preorder array
+        recursively build tree by passing in new preorder and inorder arrays to buildtree
 
-        i: 2 int arrays
-        o: root node of constructed btree using array nodes
-        c: none
-        e: 
-        
-        preorder = [3,9,20,15,7]
-
-        inorder = [9,3,15,20,7]
         '''
-        # build root
-        if not preorder:
+        if not preorder or not inorder:
             return None
 
-        if not inorder:
-            return None
+        root = preorder[0]
+        pivot = inorder.index(root)
+        inorderLeft = inorder[:pivot]
+        inorderRight = inorder[pivot+1:]
+        preorderLeft = preorder[1:1+len(inorderLeft)]
+        preorderRight = preorder[1+len(inorderLeft):]
+        
+        left = self.buildTree(preorderLeft, inorderLeft)
+        right = self.buildTree(preorderRight, inorderRight)
 
-        root = TreeNode(preorder[0])
+        res = TreeNode(root)
+        res.left = left
+        res.right = right
+        
+        return res
+        
 
-        rootIdx = inorder.index(preorder[0]) 
-        leftTree = inorder[:rootIdx]
-        rightTree = inorder[rootIdx + 1:]
-
-        szleft = len(leftTree)
-        szright = len(rightTree)
-        
-        preorderLeft = preorder[1: 1 + szleft]
-        preorderRight = preorder[1 + szleft:]
-        
-        # buildTree takes in 2 arrays
-        left = self.buildTree(preorderLeft, leftTree)
-        root.left = left
-        # buildTree takes in 2 arrays
-        right = self.buildTree(preorderRight, rightTree)
-        root.right = right
-        
-        # return root
-        return root
-        
