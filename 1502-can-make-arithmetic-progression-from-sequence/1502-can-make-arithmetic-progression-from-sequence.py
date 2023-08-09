874d96f629bb9c1b3a5e2dae31bf677dbe84b179
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2023-08-09 15:36:31 -0700

    Time: 44 ms (94.93%), Space: 16.6 MB (12.16%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -12,18 +12,37 @@ class Solution:
         [1 3 5]
 
         [4, 1, 10, 7] -> true
+
+
+        no sorting
+        use set
+        store differences in set
+        store initial diff in set
+        before adding to set, check inclusion, if diff isn't in set, return false
         '''
-        
-        arrSorted = sorted(arr)
 
-        l, r, = 0, 1
-        diff = arrSorted[r] - arrSorted[l]
-        while r < len(arrSorted):
-            if (arrSorted[r] - arrSorted[l]) != diff:
+        min_value, max_value = min(arr), max(arr)
+        n = len(arr) 
+        
+        # if min and max value are the same, every element is the same, return True
+        if max_value - min_value == 0:
+            return True
+        
+        # can think of n - 1 as # of gaps in the array. [3, (GAP) 5, (GAP) 1] 5 - 1 % 2 -> 0
+        if (max_value - min_value) % (n - 1) != 0:
+            return False
+        
+        # establish an initial diff we use to determine the result
+        diff = (max_value - min_value) // (n - 1)
+
+
+        number_set = set()
+        
+        for a in arr:
+            if a in number_set:
                 return False
-
-            l += 1
-            r += 1
-
-        return True
+            if (a - min_value) % diff != 0:
+                return False
+            number_set.add(a)
         
+        return True
