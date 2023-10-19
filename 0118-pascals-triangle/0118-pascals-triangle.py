77f242921ec61eff2ddb45b26f19776e3a13db9a
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2023-10-19 11:55:34 -0700

    Time: 34 ms (90.99%), Space: 16.3 MB (61.83%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,13 +1,26 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        '''
+        i loops from 0 -> 3 because we've already filled the first subarray. i iterates 4 times
+        j loops subarray length. append j times.
+
+        my way:
+        we loop based on positions
+
+        i iterates from 1 to 4, representing the res array slots to fill
+        j iterates 0 to length of subarray, filling in slots at index j
+
+
+        '''
+
         res = [[1]]
 
-        for i in range(numRows - 1):
-            prevRow = [0] + res[i] + [0]
-            currRow = []
+        for i in range(1, numRows):
+            temp = [0] + res[i - 1] + [0] # pad outer with extra 0s. 
+            curr = [1] * (len(res[i - 1]) + 1)
+            for j in range(len(curr)):
+                curr[j] = temp[j] + temp[j + 1]
 
-            for j in range(len(res[i]) + 1):
-                currRow.append(prevRow[j] + prevRow[j+1])
-            res.append(currRow)
+            res.append(curr)
 
         return res
