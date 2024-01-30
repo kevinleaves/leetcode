a21cdc0c1b373575349beea4d8b91636d0eb1278
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2024-01-30 10:00:09 -0800

    Time: 45 ms (22.92%), Space: 16.6 MB (62.13%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,34 +1,18 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        '''
-        i: int[], val: int
-        o: int[] w/ vals removed and all non-val integers moved to the left side. same len as num
-        c: in place so O(1) space
-        e: 
-        '''
-        # instantiate left and right pointers
-        # left at 0
-        # right at last idx of array
-        l, r = 0, len(nums) - 1
-        count = 0
-        # while left <= right
-        while l <= r:
-          # while right is equal to val
-          while r >= 0 and nums[r] == val:
-            # decrement right
-            r -= 1
-            count += 1
+        if len(nums) == 0:
+          return 0
 
-          # if nums[left] is val
-          if l <= r and nums[l] == val:
-            # swap nums at left and right
-            nums[l], nums[r] = nums[r], nums[l]
-            # decrement right
-            r -= 1
-            count += 1
-        
-          # increment left
-          l += 1
-        return len(nums) - count
-        
-        
+        l = 0
+        r = 0
+
+        while r < len(nums):
+          # conditional
+          if nums[r] != val:
+            # overwrite L with R
+            nums[l] = nums[r]
+            l += 1
+          
+          r += 1
+          
+        return l
