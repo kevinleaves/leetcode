7eb662e03ecf9e4da7fd27de825681a9b5a32e34
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2024-02-06 13:32:08 -0800

    Time: 59 ms (54.13%), Space: 18.4 MB (79.34%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
@@ -6,17 +6,34 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        if not root:
-            return None
+        '''
+        i: root of bst, val int to search for
+        o: node with the target val. return that node/subtree
+        c: none
+        e: node does not exist within the bst, return null
 
-        # search left
-        if root.val > val:
-            left = self.searchBST(root.left, val)
-            return left
-        # search right
-        elif root.val < val:
-            right = self.searchBST(root.right, val)
-            return right
+        bst properties mean we only need to search 1/2 of the subtree based on the val
+
+        establish curr pointer at root
+        if val < node.val:
+          traverse left (move pter)
+        else if val > node.val:
+          traverse right (move pter)
         else:
-            return root
+          # val === node.val
+          return node
+
+        '''
+
+        curr = root
+        while curr:
+          if val == curr.val:
+            return curr
+          elif val > curr.val:
+            curr = curr.right
+          else:
+            curr = curr.left
+        
+        # if we don't return anything and we reach the end of the traversal return null
+        return None
 
