4217776b034c85792492b3068b6be64b0dbbaf43
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2024-02-21 16:56:23 -0800

    Time: 63 ms (97.78%), Space: 16.6 MB (82.67%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
@@ -1,29 +1,23 @@
 class Solution:
-    def numberOfPoints(self, nums: List[List[int]]) -> int:
-        '''
-        i: 2d array nums. at each index is a car. car includes range that it covers within the line
-        o: int representing the # of points that are covered by all the cars. from 1 -> max(nums)
-        c: none
-        e: numbers cannot be negative. nums len >= 1
+    def numberOfPoints(self, nums):
+        N = 102 # number of line segments
+        line = [0] * N
 
-        total int points on the line are in the range 1 to max int in nums
-        how many of these points are covered
+        for segment in nums:
+            start_point, end_point = segment
+            # Increment count at the start point of the segment
+            line[start_point] += 1
+
+            # Decrement count at the point after the end point of the segment
+            line[end_point + 1] -= 1
+
+        points_covered = 0 
+        segment_accumulator = 0
+        for count in line:            
+            segment_accumulator += count            
+            # if there is at least 1 segment covering the current point
+            if segment_accumulator >= 1:
+                points_covered += 1
 
 
-        {1,2,3,5,6,7,8}
-        '''
-        # O(N x M) time, where N is the number of cars, and M is the maximum distance that the car covers
-        # O(N) space
-
-        # initialize a set
-        points = set()
-        # iterate through nums, 
-        for car in nums:
-          # at each car array, iterate through that car's start and end range (inclusive)
-          for point in range(car[0], car[1]+1):
-            #  add point to the set
-            points.add(point)
-
-        # return length of set
-        return len(points)
-
+        return points_covered
