05aa59bb73eaf460de1892325706fc4e46814a08
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2024-02-22 17:06:32 -0800

    Time: 172 ms (41.28%), Space: 23.8 MB (72.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -2,8 +2,6 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         # O(N) space, O(N) time
         prefixes = [0]*len(nums)
-        suffixes = [0]*len(nums)
-
         runningPrefixProduct = 1
         for i, num in enumerate(nums):
           # update prefix array
@@ -13,11 +11,8 @@ class Solution:
 
         runningSuffixProduct = 1
         for j in range(len(nums) - 1, -1,-1):
-          suffixes[j] = runningSuffixProduct
+          prefixes[j] *= runningSuffixProduct
           runningSuffixProduct *= nums[j]
 
-        for k in range(len(prefixes)):
-          prefixes[k] *= suffixes[k]
-        
         return prefixes
         
