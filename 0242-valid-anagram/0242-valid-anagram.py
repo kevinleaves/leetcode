cadf924b779a1068aaa041745f784613e2e98b98
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2024-02-22 17:23:31 -0800

    Time: 60 ms (19.09%), Space: 17.1 MB (63.90%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,24 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        # dictionary to hold character: frequency that character occurs in s
-        frequency = {}
+      if len(s) != len(t):
+        return False
 
-        # iterate through chars of s
-        for c in s:
-            # update key with current frequency
-            frequency[c] = frequency.get(c, 0) + 1
-        
-        print(frequency)
-        for c in t:
-            if c not in frequency or frequency[c] <= 0:
-                return False
-            else:
-                frequency[c] -= 1
+      freq = collections.Counter()
+      for c in s:
+        freq[c] += 1
+      
+      for c in t:
+        if freq[c] == 0:
+          return False
+        freq[c] -= 1
 
-        return True
-                
+      return True
+      
 
-
-        
