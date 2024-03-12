35ef913ef9839728eea3f5e8ef0bed63e0a50336
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2024-03-12 11:56:41 -0700

    Time: 59 ms (33.51%), Space: 16.6 MB (55.53%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,32 +4,57 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def reverseList(self, head: ListNode) -> [ListNode, int]:
+      curr = head
+      prev = None
+      n = 0
+      while curr:
+        tmp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = tmp
+        n += 1
+      return [prev, n]
+
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        reverse both lists, while maintaining the length of both lists
+        use both list n's to convert list to an int
+        add ints together
+        split the int into a linked list
+        reverse that list and return it
+        '''
+        l1, n1 = self.reverseList(l1)
+        l2, n2 = self.reverseList(l2)
+        sum1 = 0
+        sum2 = 0
+
+        curr1 = l1
+        while curr1:
+          sum1 += curr1.val * (10 ** (n1-1))
+          n1 -= 1
+          curr1 = curr1.next
+
+        curr2 = l2
+        while curr2:
+          sum2 += curr2.val * (10 ** (n2-1))
+          n2 -= 1
+          curr2 = curr2.next
+
+        resSum = sum1 + sum2
+        if resSum == 0:
+          return ListNode(0)
+          
         dummy = ListNode()
         curr = dummy
-        carry = 0
+        while resSum > 0:
+          digit = resSum % 10
+          curr.next = ListNode(digit)
+          curr = curr.next
+          resSum //= 10
 
-        # continue loop until we've traversed l1 and l2 fully and until carry is 0
-        while l1 or l2 or carry == 1:
-            # if else conditions handle edge case of numbers having mismatching digits
-            x = l1.val if l1 else 0
-            y = l2.val if l2 else 0
-
-            # elementary school math
-            sum = x + y + carry
-
-            # calculate a new carry value for the next iteration (could be 0 or 1)
-            carry = sum // 10
-            
-            # make a new node, calculated from the summed value
-            curr.next = ListNode(sum % 10)
-
-            # advance l1 & l2 pointers if the nodes exist, otherwise they're Null
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        
-            curr = curr.next
         return dummy.next
-            
 
-            
+        
+
+    
