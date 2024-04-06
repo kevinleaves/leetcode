6de26b8ca9153bcb2806bf6753723572e1803aa5
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2024-04-05 20:05:47 -0700

    Time: 40 ms (36.40%), Space: 16.6 MB (42.29%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,15 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # SELECTION SORT (ON^2)
-        writeIdx = 0
-        while writeIdx < len(nums):
-          # find index of the smallest number          
-          lowestValIdx = writeIdx
-          for i in range(writeIdx, len(nums)):
-            if nums[i] < nums[lowestValIdx]:
-              lowestValIdx = i
-          nums[writeIdx], nums[lowestValIdx] = nums[lowestValIdx], nums[writeIdx]
-          writeIdx += 1
+        # INSERTION SORT
+        # TIME: O(N^2) Worst Case & Average Case. O(N) Best Case
+        # SPACE: O(1) because in place.
+
+        # start with the second index
+        # compare current item with its preceding elements, stopping when it reaches an element it is smaller than
+        # once we've found our insertion point, insert
+        for i in range(1, len(nums)):
+          tmp = nums[i]
+          j = i - 1
+          # break when nums[j] is smaller than tmp
+          while j >= 0 and nums[j] > tmp:
+            # shift elements to the right
+            nums[j + 1] = nums[j]
+            j -= 1
+          # we have a hole to right of our current j. this is where tmp goes
+          nums[j + 1] = tmp
         
+
+            
+
+
+
         
