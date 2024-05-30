99d4b25e6ce5602082fad937247cfd7e436cb3bc
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2024-05-30 13:23:08 -0700

    Time: 29 ms (90.00%), Space: 16.5 MB (76.32%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -6,37 +6,17 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # node, left, right
-        # process first, explore left nodes, explore right nodes.
-        # iterative => emulate recursion using a stack + pointer
+      res = []
+      def dfs(node: Optional[TreeNode]) -> None:
+        if not node:
+          return None
+        
+        # process
+        res.append(node.val)
+        # left
+        dfs(node.left)
+        dfs(node.right)
+        # right
 
-        res = []
-        curr = root
-        stack = []
-
-        '''
-        [1 2 4 5 6]
-        res = [1 2 4]
-        stack =[3 5]
-         1
-        2 3
-       4 5
-          6
-
-        where i went wrong? i was missing the condition of when curr == None. didn't identify that i either move left OR i pop to return right. we're also able to add null nodes here because we take care of that scenario in our base case
-        '''
-        while stack or curr:
-            # when do we pop? when we're done going left. notice we have no while loops here
-            if not curr:
-                curr = stack.pop()
-            else:
-                # process node
-                res.append(curr.val)
-                
-                # add right node so we can pop back to right
-                stack.append(curr.right)
-
-                # go left. if curr == none that's base case, then we pop and go right
-                curr = curr.left
-
-        return res
+      dfs(root)
+      return res
