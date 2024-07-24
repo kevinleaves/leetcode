7bf56080b08cc4a25876f737253cda3833b05469
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2024-07-24 12:20:00 -0700

    Time: 34 ms (67.61%), Space: 16.4 MB (95.61%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,38 +1,28 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-      '''
-      cases: 
-        w1 len == w2 len
-        w1 len > w2 len
-        w1 len < w2 len
+        # happy path -> both words are equal length
+        s = ''
+        if len(word1) == len(word2):
+          for i in range(len(word1)):
+            s += word1[i]
+            s += word2[i]
+          return s
 
-      want to hit every element
+        # unhappy path -> words are != length
+        if len(word1) < len(word2):
+          i = 0
+          while i < len(word1):
+            s += word1[i]
+            s += word2[i]
+            i += 1
+          s += word2[i:]
+          return s
 
-      no constraints on time and space
-      can build resulting string
-
-      alternating starting with w1
-      
-
-      add w1[0]
-      add w2[0]
-      '''
-
-      res = ''
-      position = 0
-      # outer runs as long as we haven't hit all elements
-      while position < len(word1) or position < len(word2): 
-        res += word1[position]
-        res += word2[position]
-        position += 1
-        
-        # w1 is longer, add the rest of w1
-        if position >= len(word2):
-          res += word1[position:]
-          return res
-        elif position >= len(word1):
-        # w2 is longer
-          res += word2[position:]
-          return res
-
-      return res
+        if len(word1) > len(word2):
+          i = 0
+          while i < len(word2):
+            s += word1[i]
+            s += word2[i]
+            i += 1
+          s += word1[i:]
+          return s
