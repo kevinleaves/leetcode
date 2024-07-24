ad89b1db717d38aa2b17f31254e8cb10ca647b5c
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2024-07-24 12:20:17 -0700

    Time: 760 ms (40.69%), Space: 27.5 MB (22.56%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,25 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         '''
-        template:
-        init res
-        expand window
-        contract window when x condition
-          find out where left goes
-
-        update max/maintain answer/diff btwn right and left
-        return res
+        i: array of prices of given stock on the ith day
+        o: int max profit we can achieve in 1 transaction, return 0 if no profit can be gained
+        c: none
+        e: can't return profit -> return 0. if array length is 1, return 0 because we can't buy and sell on
+        the same day
         '''
 
-        left = 0
         maxProfit = 0
-        for i, price in enumerate(prices):
-          currProfit = prices[i] - prices[left]
-          if currProfit < 0:
-            # contract window, move left pointer. where? +1? 
-            # no, shift it all the way over to i, because we want left to be pointing to the lowest possible number
-            left = i
 
+        # 2 pointers
+        # keep a pointer at the minimum price we see
+        # if current pointer - min > maxProfit, update max profit
+
+        l, r = 0, 0
+        minPrice = prices[l]
+        # while indices are in bounds
+        while l < len(prices) and r < len(prices):
+          # update minimum price
+          if prices[r] < minPrice:
+            minPrice = prices[r]
+            l = r
+
+          currProfit = prices[r] - prices[l]
           maxProfit = max(maxProfit, currProfit)
+          r += 1
 
         return maxProfit
