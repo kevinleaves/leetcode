65e3b3e93e65940c1923eed6448fca4a3817bdd3
Kevin Le <60834712+kevinleaves@users.noreply.github.com>
2024-08-05 15:50:48 -0700

    Time: 38 ms (39.77%), Space: 16.6 MB (11.26%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,38 +1,54 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        '''
-        i: string with various bracket characters
-        o: boolean. true if input string is valid.
-        c: o(n) time? no space constraints
-        e: s length could be 1. odd s length vs even s length
+      '''
+      i: string with various bracket characters
+      o: boolean. true if input string is valid.
+      c: o(n) time? no space constraints
+      e: s length could be 1. odd s length vs even s length
 
-        how do we know that we use a stack?
-        order of operations.
-        check for validity by iterating through a string or an array. like a calculator or checking for valid brackets. 
-        pattern: 
-        if x happens, pop, else push, etc. etc.
-        can determine answers based on remaining length of stack after operations, sum of items remaining in stack, etc
-        '''
+      how do we know that we use a stack?
+      order of operations.
+      check for validity by iterating through a string or an array. like a calculator or checking for valid brackets. 
+      pattern: 
+      if x happens, pop, else push, etc. etc.
+      can determine answers based on remaining length of stack after operations, sum of items remaining in stack, etc
 
-        stack = []
-        brackets = dict([
-          (')', "("),
-          ('}', "{"),
-          (']',"["),
-        ])
+      if odd length string, immediately return false
+      '''
 
-        for char in s:
-          # if char is an open bracket, push to stack
-          if char not in brackets:
-            stack.append(char)
+      if len(s) % 2 == 1:
+        return False
 
-          # otherwise:
-          else:
-            # peek stack, if closing bracket doesn't correspond,
-            if len(stack) == 0 or brackets[char] != stack[-1]:
-              return False
-            else:
-              stack.pop()
+      # create a mapping
+      # create a stack
+      stack = []
+      mapping = {
+        "]": "[",
+        ")": "(",
+        "}": "{",
+      }
+      # s = "}{" => false -> encounter closing bracket when stack is empty
+      # s = "(){" => false -> odd length s
+      # s = "(){{" => false -> stack isn't empty at end of iteration
+      # s = "(]" => closing bracket doesn't match top of stack
+      
+      # iterate through s
+      for c in s:
+        # if close bracket
+        if c in mapping:
+          # check top of stack to see if it's a pair
+          if len(stack) == 0:
+            return False
+          # if not,
+            # return false
+          if stack[-1] != mapping[c]:
+            return False
+          stack.pop()
+        # if open bracket, add to stack
+        else:
+          stack.append(c)
+      # if stack is empty, return true, else false
+      
+
+      return len(stack) == 0
         
-        return len(stack) == 0 
-            
